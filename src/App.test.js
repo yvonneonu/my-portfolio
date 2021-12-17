2156a318774e9e5ff5cb66004a48f7cfcaf8727c
yvonneonu <yvonneonuorah@gmail.com>
2021-12-17 10:55:43 +0100

    iyhi

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders learn react link', () => {
-  render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-  expect(screen.getByAltText('Learn React')).toBeInTheDocument();
+test('renders learn react link', () => {
+  render(<App/>);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
