8efdfd531fadabf5ab4142856609a9f94fb73934
yvonneonu <yvonneonuorah@gmail.com>
2021-12-17 05:15:55 +0100

    upnew

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -32,7 +32,7 @@ function Copyright() {
     <Typography variant="body2" color="text.secondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="#">
-        My Portfolios
+        My Portfolio
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
